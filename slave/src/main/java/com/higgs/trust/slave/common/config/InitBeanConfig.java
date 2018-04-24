f2863ff0b720eb832f522e2f11a8b322bb57d460
彭迪 <pengdi@primeledger.cn>
2018-04-24 23:45:19 +0800

    commit p2p config

--- slave/src/main/java/com/higgs/trust/slave/common/config/InitBeanConfig.java
+++ slave/src/main/java/com/higgs/trust/slave/common/config/InitBeanConfig.java
@@ -1,12 +1,19 @@
 package com.higgs.trust.slave.common.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.cloud.sleuth.instrument.async.TraceableExecutorService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 /**
@@ -22,5 +29,19 @@ import java.util.concurrent.*;
             new ThreadPoolExecutor(1, 1, 0L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1024), namedThreadFactory,
                 new ThreadPoolExecutor.AbortPolicy());
         return new TraceableExecutorService(beanFactory, packageExecutor);
+    }
+
+    @Bean public HttpMessageConverters HttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect,
+            SerializerFeature.WriteMapNullValue, SerializerFeature.SortField, SerializerFeature.MapSortField,
+            SerializerFeature.WriteClassName);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        List<MediaType> supportedMediaTypes = new ArrayList<>();
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+        supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(supportedMediaTypes);
+        return new HttpMessageConverters(fastConverter);
     }
 }
--- slave/src/main/java/com/higgs/trust/slave/model/bo/consensus/ValidateCommand.java
+++ slave/src/main/java/com/higgs/trust/slave/model/bo/consensus/ValidateCommand.java
@@ -2,20 +2,22 @@ package com.higgs.trust.slave.model.bo.consensus;
 
 import com.higgs.trust.consensus.p2pvalid.core.ValidCommand;
 import com.higgs.trust.slave.model.bo.BlockHeader;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * @Description:validate p2p command for consensus
  * @author: pengdi
  **/
-public class ValidateCommand extends ValidCommand<BlockHeader> {
+@Getter @Setter @NoArgsConstructor public class ValidateCommand extends ValidCommand<BlockHeader> {
     private static final long serialVersionUID = -1L;
 
     public ValidateCommand(Long seqNum, BlockHeader header) {
         super(header);
     }
 
-    @Override
-    public String messageDigest() {
+    @Override public String messageDigest() {
         return get().toString();
     }
 }
