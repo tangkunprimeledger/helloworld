899f4f7b78899137b9de28b2ac9c3d87b87ef1dc
彭迪 <pengdi@primeledger.cn>
2018-04-26 01:00:10 +0800

    for test

---
+++ consensus/src/main/java/com/higgs/trust/consensus/test/StringStateMachine.java
@@ -0,0 +1,24 @@
+package com.higgs.trust.consensus.test;
+
+import com.higgs.trust.consensus.bft.core.ConsensusCommit;
+import com.higgs.trust.consensus.bft.core.template.AbstractConsensusStateMachine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StringStateMachine extends AbstractConsensusStateMachine {
+    private static final Logger log = LoggerFactory.getLogger(StringStateMachine.class);
+
+    /**
+     * apply method for your command
+     * @return
+     */
+    public String stringApply(ConsensusCommit<StringCommand> commit) {
+        try {
+            log.warn("command value is {}", commit.operation().get());
+            return commit.operation().get();
+        } finally {
+            System.out.println("close the commit");
+            commit.close();
+        }
+    }
+}
