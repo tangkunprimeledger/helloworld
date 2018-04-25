e37ab29fe9384dd02794433c6527078317d7dd2d
程文衍 <chengwenyan@primeledger.cn>
2018-04-25 21:24:05 +0800

    调整心跳

--- consensus/src/main/java/com/higgs/trust/consensus/bft/config/ServerConfig.java
+++ consensus/src/main/java/com/higgs/trust/consensus/bft/config/ServerConfig.java
@@ -96,9 +96,9 @@ public class ServerConfig
         });
 
         builder.withTransport(NettyTransport.builder().withThreads(nettyThreadNum).build());
-        builder.withHeartbeatInterval(Duration.ofMillis(1000))
-                .withElectionTimeout(Duration.ofMillis(2000))
-                .withSessionTimeout(Duration.ofMillis(4000));
+        builder.withHeartbeatInterval(Duration.ofMillis(500))
+                .withElectionTimeout(Duration.ofMillis(1500))
+                .withSessionTimeout(Duration.ofMillis(5000));
 
         Storage storage = Storage.builder().withStorageLevel(StorageLevel.DISK).withDirectory(logDir)
                 .withMinorCompactionInterval(Duration.ofMillis(minorCompactionInterval))
