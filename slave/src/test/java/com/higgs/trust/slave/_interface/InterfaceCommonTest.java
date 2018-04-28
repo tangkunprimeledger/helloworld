0b135dca4752a33921f374770290a868e8e7a1ea
liuyu <liuyu@primeledger.cn>
2018-04-28 11:17:37 +0800

    test modify

--- slave/src/test/java/com/higgs/trust/slave/_interface/InterfaceCommonTest.java
+++ slave/src/test/java/com/higgs/trust/slave/_interface/InterfaceCommonTest.java
@@ -3,7 +3,9 @@ package com.higgs.trust.slave._interface;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.higgs.trust.slave.BaseTest;
+import com.higgs.trust.slave.JsonFileUtil;
 import com.higgs.trust.slave.api.enums.manage.InitPolicyEnum;
+import com.higgs.trust.slave.core.service.action.ActionHandler;
 import com.higgs.trust.slave.core.service.action.account.TestDataMaker;
 import com.higgs.trust.slave.core.service.snapshot.SnapshotService;
 import com.higgs.trust.slave.model.bo.*;
@@ -11,47 +13,119 @@ import com.higgs.trust.slave.model.bo.Package;
 import com.higgs.trust.slave.model.bo.action.Action;
 import com.higgs.trust.slave.model.bo.context.PackContext;
 import com.higgs.trust.slave.model.enums.biz.PackageStatusEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * @author liuyu
  * @description
  * @date 2018-04-26
  */
-public class InterfaceCommonTest extends BaseTest{
+@Slf4j public abstract class InterfaceCommonTest extends BaseTest {
+    /**
+     * 数据库连接定义
+     */
+    public static String DB_URL = "jdbc:mysql://localhost:3306/trust?user=root&password=root";
+    /**
+     * snapshot service
+     */
     @Autowired SnapshotService snapshotService;
 
-    @BeforeMethod
-    public void before(){
+    @BeforeMethod public void before() {
         snapshotService.startTransaction();
     }
 
-    @AfterMethod
-    public void after(){
+    @AfterMethod public void after() {
         snapshotService.destroy();
     }
 
-    public <T> T getBodyData(Map<?, ?> param,Class<T> clazz){
-        String body = String.valueOf(param.get("body"));
-        if(StringUtils.isEmpty(body) || "null".equals(body)){
-            return null;
-        }
-        body = body.replaceAll(JSONObject.class.getCanonicalName(),clazz.getCanonicalName());
-        return JSON.parseObject(body,clazz);
+    /**
+     * 测试数据根路径
+     *
+     * @return
+     */
+    protected abstract String getProviderRootPath();
+
+    /**
+     * 默认的测试数据源，路径：测试数据根路径+测试方法名
+     *
+     * @param method
+     * @return
+     */
+    @DataProvider public Object[][] defaultProvider(Method method) {
+        String name = method.getName();
+        String providerPath = getProviderRootPath() + name;
+        log.info("[defaultProvider].path:{}", providerPath);
+        String filePath = JsonFileUtil.findJsonFile(providerPath);
+        HashMap<String, String>[][] arrMap = (HashMap<String, String>[][])JsonFileUtil.jsonFileToArry(filePath);
+        return arrMap;
     }
 
-    public String getAssertData(Map<?, ?> param){
+    /**
+     * 执行actionHandler, validate、persist都会执行
+     *
+     * @param param
+     * @param actionHandler
+     * @param action
+     */
+    protected void executeActionHandler(Map<?, ?> param,ActionHandler actionHandler,Action action){
+        String assertData = getAssertData(param);
+        try {
+            actionHandler.validate(makePackContext(action, 1L));
+            actionHandler.persist(makePackContext(action, 1L));
+        }catch (Exception e){
+            log.info("has error:{}",e.getMessage());
+            assertEquals(e.getMessage(),assertData);
+        }
+    }
+
+    /**
+     * 获取 测试数据中的 body 对象实体
+     * @param param
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    protected <T> T getBodyData(Map<?, ?> param, Class<T> clazz) {
+        String body = String.valueOf(param.get("body"));
+        if (StringUtils.isEmpty(body) || "null".equals(body)) {
+            return null;
+        }
+        body = body.replaceAll(JSONObject.class.getCanonicalName(), clazz.getCanonicalName());
+        return JSON.parseObject(body, clazz);
+    }
+
+    /**
+     * 断言
+     *
+     * @param param
+     * @return
+     */
+    protected String getAssertData(Map<?, ?> param) {
         return String.valueOf(param.get("assert"));
     }
 
-    public PackContext makePackContext(Action action,Long blockHeight) throws Exception {
+    /**
+     * package data 的 封装
+     *
+     * @param action
+     * @param blockHeight
+     * @return
+     * @throws Exception
+     */
+    protected PackContext makePackContext(Action action, Long blockHeight) throws Exception {
         List<Action> actions = new ArrayList<>();
         actions.add(action);
         CoreTransaction coreTransaction = TestDataMaker.makeCoreTx(actions, 1, InitPolicyEnum.REGISTER);
--- slave/src/test/java/com/higgs/trust/slave/_interface/accounting/OperationAccountTest.java
+++ slave/src/test/java/com/higgs/trust/slave/_interface/accounting/OperationAccountTest.java
@@ -2,16 +2,13 @@ package com.higgs.trust.slave._interface.accounting;
 
 import com.higgs.trust.slave._interface.InterfaceCommonTest;
 import com.higgs.trust.slave.api.enums.ActionTypeEnum;
-import com.higgs.trust.slave.core.service.action.account.IssueCurrencyHandler;
+import com.higgs.trust.slave.core.service.action.account.AccountOperationHandler;
 import com.higgs.trust.slave.model.bo.account.IssueCurrency;
 import com.higgs.trust.tester.dbunit.DataBaseManager;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
@@ -19,28 +16,20 @@ import java.util.Map;
  * @description
  * @date 2018-04-26
  */
-@Slf4j public class IssueCurrencyTest extends InterfaceCommonTest {
-    private static String PROVIDER_ROOT_PATH = "java/com/higgs/trust/slave/core/service/accounting/issueCurrency/";
+@Slf4j public class OperationAccountTest extends InterfaceCommonTest {
+    private static String PROVIDER_ROOT_PATH = "java/com/higgs/trust/slave/core/service/accounting/operationAccount/";
 
-    @Autowired IssueCurrencyHandler issueCurrencyHandler;
+    @Autowired AccountOperationHandler accountOperationHandler;
 
     @Override protected String getProviderRootPath() {
         return PROVIDER_ROOT_PATH;
     }
 
-    @AfterMethod
-    public void after(Method method){
-        super.after();
-        String name = method.getName();
-        if(!StringUtils.equals("testRegular",name)){
-            clearDB();
-        }
-    }
-
     private void clearDB(){
-        String sql = "truncate table currency_info;";
+        StringBuilder sql = new StringBuilder();
+        sql.append("truncate table account_info;");
         DataBaseManager dataBaseManager = new DataBaseManager();
-        dataBaseManager.executeSingleDelete(sql,DB_URL);
+        dataBaseManager.executeSingleDelete(sql.toString(),DB_URL);
     }
 
     @Test(dataProvider = "defaultProvider", priority = 1) public void paramValidate(Map<?, ?> param){
@@ -50,7 +39,7 @@ import java.util.Map;
             issueCurrency.setType(ActionTypeEnum.ISSUE_CURRENCY);
             issueCurrency.setIndex(1);
         }
-        executeActionHandler(param,issueCurrencyHandler,issueCurrency);
+        executeActionHandler(param,accountOperationHandler,issueCurrency);
     }
 
     @Test(dataProvider = "defaultProvider", priority = 2) public void testRegular(Map<?, ?> param){
@@ -60,7 +49,7 @@ import java.util.Map;
             issueCurrency.setType(ActionTypeEnum.ISSUE_CURRENCY);
             issueCurrency.setIndex(1);
         }
-        executeActionHandler(param,issueCurrencyHandler,issueCurrency);
+        executeActionHandler(param,accountOperationHandler,issueCurrency);
     }
 
     @Test(dataProvider = "defaultProvider", priority = 3) public void testException(Map<?, ?> param){
@@ -70,7 +59,7 @@ import java.util.Map;
             issueCurrency.setType(ActionTypeEnum.ISSUE_CURRENCY);
             issueCurrency.setIndex(1);
         }
-        executeActionHandler(param,issueCurrencyHandler,issueCurrency);
+        executeActionHandler(param,accountOperationHandler,issueCurrency);
     }
 
 }
