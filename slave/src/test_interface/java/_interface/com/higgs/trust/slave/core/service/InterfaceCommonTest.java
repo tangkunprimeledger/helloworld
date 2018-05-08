aa3ad01951ed683c8166c0693df509d15b1eebbe
zyf <zhouyafeng@primeledger.cn>
2018-05-08 10:45:40 +0800

    新建接口测试包

--- slave/src/test_interface/java/_interface/com/higgs/trust/slave/core/service/InterfaceCommonTest.java
+++ slave/src/test_interface/java/_interface/com/higgs/trust/slave/core/service/InterfaceCommonTest.java
@@ -1,11 +1,14 @@
-package com.higgs.trust.slave;
+package _interface.com.higgs.trust.slave.core.service;
 
 import com.alibaba.fastjson.JSON;
+import com.higgs.trust.slave.BaseTest;
+import com.higgs.trust.slave.JsonFileUtil;
 import com.higgs.trust.slave.api.enums.ActionTypeEnum;
 import com.higgs.trust.slave.api.enums.manage.InitPolicyEnum;
 import com.higgs.trust.slave.core.service.action.ActionHandler;
 import com.higgs.trust.slave.core.service.action.account.TestDataMaker;
 import com.higgs.trust.slave.core.service.snapshot.SnapshotService;
+import com.higgs.trust.slave.model.bo.*;
 import com.higgs.trust.slave.model.bo.Package;
 import com.higgs.trust.slave.model.bo.action.Action;
 import com.higgs.trust.slave.model.bo.context.PackContext;
--- slave/src/test_interface/java/_interface/com/higgs/trust/slave/core/service/merkle/MerkleServiceImplTest.java
+++ slave/src/test_interface/java/_interface/com/higgs/trust/slave/core/service/merkle/MerkleServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.higgs.trust.slave.core.service.merkle;
+package _interface.com.higgs.trust.slave.core.service.merkle;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -7,6 +7,7 @@ import com.higgs.trust.slave.BaseTest;
 import com.higgs.trust.slave.JsonFileUtil;
 import com.higgs.trust.slave.api.enums.MerkleTypeEnum;
 import com.higgs.trust.slave.common.exception.MerkleException;
+import com.higgs.trust.slave.core.service.merkle.MerkleService;
 import com.higgs.trust.slave.model.bo.merkle.MerkleTree;
 import com.higgs.trust.tester.dbunit.DataBaseManager;
 import org.springframework.beans.factory.annotation.Autowired;
