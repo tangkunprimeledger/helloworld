971391f2b3e6cfef4031e00fea85a2f4d6cf7e9a
程文衍 <chengwenyan@primeledger.cn>
2018-05-10 11:31:11 +0800

    bugfix 1、数据库死锁 2、发送状态修改导致消息丢失

--- consensus/src/main/java/com/higgs/trust/consensus/p2pvalid/dao/SendNodeDao.java
+++ consensus/src/main/java/com/higgs/trust/consensus/p2pvalid/dao/SendNodeDao.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface SendNodeDao extends BaseConsensusDao<SendNodePO> {
     public List<SendNodePO> queryByDigestAndStatus(@Param("messageDigest") String messageDigest, @Param("status") Integer status);
     public int countByDigestAndStatus(@Param("messageDigest") String messageDigest, @Param("status") Integer status);
-    public int transStatus(@Param("messageDigest") String messageDigest, @Param("from") Integer from, @Param("status") Integer status);
+    public int transStatus(@Param("messageDigest") String messageDigest, @Param("toNodeName") String toNodeName, @Param("from") Integer from, @Param("status") Integer status);
 }
