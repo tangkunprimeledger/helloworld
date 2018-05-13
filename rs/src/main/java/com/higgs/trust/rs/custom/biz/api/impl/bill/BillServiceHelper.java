b13b51a05fa4e0fbce38b60ec637746a317ab52a
lingchao <lingchao@primeledger.cn>
2018-05-13 16:21:41 +0800

    logic

--- rs/src/main/java/com/higgs/trust/rs/custom/biz/api/impl/bill/BillServiceHelper.java
+++ rs/src/main/java/com/higgs/trust/rs/custom/biz/api/impl/bill/BillServiceHelper.java
@@ -1,5 +1,6 @@
 package com.higgs.trust.rs.custom.biz.api.impl.bill;
 
+import com.higgs.trust.rs.custom.model.RespData;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,4 +11,26 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BillServiceHelper {
+
+/*
+    public RespData<?> requestIdempotent(String request_id) {
+    */
+/*    RespData<?> respData = null;
+        Transaction transactionPO = transactionDao.queryByTxId(txId);
+        if (null != transactionPO) {
+            if (StringUtils.equals(transactionPO.getStatus(), TransferStatusEnum.INIT.getCode())) {
+                LOGGER.info("txId：{} 的交易请求幂等，交易已接收", txId);
+                respData = new RespData(RespCodeEnum.TRANSFER_ACCEPTED);
+            } else {
+                LOGGER.info("txId：{} 的交易请求幂等，交易发送失败， 交易状态为:{}", txId, transactionPO.getStatus());
+                respData = new RespData(RespCodeEnum.TRANSFER_SEND_FAIL);
+            }
+            TransactionVO transactionVO = CoinChainBOBuilder.buildTransactionVO(txId);
+            respData.setData(transactionVO);
+        }
+        return respData;*//*
+
+    }
+*/
+
 }
