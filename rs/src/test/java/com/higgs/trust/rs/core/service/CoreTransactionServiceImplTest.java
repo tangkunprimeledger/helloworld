97ed70acb357dbaf92ce5845b35100a4f3062b66
shenqingyan <shenqingyan@primeledger.cn>
2018-05-13 15:32:03 +0800

    修改IntegrateBaseTest位置

---
+++ rs/src/test/java/com/higgs/trust/rs/core/service/CoreTransactionServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.higgs.trust.rs.core.service;
+
+import com.higgs.trust.rs.IntegrateBaseTest;
+import org.testng.annotations.Test;
+
+public class CoreTransactionServiceImplTest extends IntegrateBaseTest {
+
+    @Test
+    public void testSubmitTx() throws Exception {
+
+    }
+}
