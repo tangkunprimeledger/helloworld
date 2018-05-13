df93e64619c0991fbbb65c40d2109cf6294e98c5
彭迪 <pengdi@primeledger.cn>
2018-05-13 15:14:55 +0800

    change request and receivable bill directory

--- rs/src/main/java/com/higgs/trust/rs/custom/controller/outter/v1/BillController.java
+++ rs/src/main/java/com/higgs/trust/rs/custom/controller/outter/v1/BillController.java
@@ -5,8 +5,11 @@ package com.higgs.trust.rs.custom.controller.outter.v1;
 
 import com.higgs.trust.rs.custom.vo.BillCreateVO;
 import com.higgs.trust.rs.custom.vo.BillTransferVO;
+import com.higgs.trust.slave.api.enums.RespCodeEnum;
 import com.higgs.trust.slave.api.vo.RespData;
+import com.higgs.trust.slave.asynctosync.BlockingMap;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController @Slf4j public class BillController {
 
+    @Autowired BlockingMap rsResultMap;
+
     /**
      * create bill
      *
@@ -25,7 +30,24 @@ import org.springframework.web.bind.annotation.RestController;
      * @return
      */
     @RequestMapping(value = "/create") RespData<String> create(@RequestBody BillCreateVO billCreateVO) {
-        return null;
+
+        RespData respData = null;
+
+        try {
+            respData = (RespData)rsResultMap.poll(billCreateVO.getRequestId(), 1000);
+        } catch (InterruptedException e) {
+            log.error("tx handle exception. ", e);
+            respData = new RespData();
+            respData.setCode(RespCodeEnum.SYS_FAIL.getRespCode());
+            respData.setMsg("handle transaction exception.");
+        }
+
+        if (null == respData) {
+            respData = new RespData();
+            respData.setCode(RespCodeEnum.SYS_HANDLE_TIMEOUT.getRespCode());
+            respData.setMsg("tx handle timeout");
+        }
+        return respData;
     }
 
     /**
