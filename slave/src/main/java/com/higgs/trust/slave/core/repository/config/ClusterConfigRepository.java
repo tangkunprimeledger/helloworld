a1eb97cb6394bce89c733a0a90ff264dcfc089c1
王全洲 <wangquanzhou@primeledger.cn>
2018-06-11 16:06:54 +0800

    优化CA处理流程16点06分

--- slave/src/main/java/com/higgs/trust/slave/core/repository/config/ClusterConfigRepository.java
+++ slave/src/main/java/com/higgs/trust/slave/core/repository/config/ClusterConfigRepository.java
@@ -1,12 +1,17 @@
 package com.higgs.trust.slave.core.repository.config;
 
+import com.higgs.trust.slave.common.enums.SlaveErrorEnum;
+import com.higgs.trust.slave.common.exception.SlaveException;
 import com.higgs.trust.slave.dao.config.ClusterConfigDao;
 import com.higgs.trust.slave.dao.po.config.ClusterConfigPO;
 import com.higgs.trust.slave.model.bo.config.ClusterConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @author WangQuanzhou
@@ -24,7 +29,7 @@ import org.springframework.stereotype.Repository;
      */
     public void insertClusterConfig(ClusterConfig clusterConfig) {
         ClusterConfigPO clusterConfigPO = new ClusterConfigPO();
-        BeanUtils.copyProperties(clusterConfig,clusterConfigPO);
+        BeanUtils.copyProperties(clusterConfig, clusterConfigPO);
         clusterConfigDao.insertClusterConfig(clusterConfigPO);
     }
 
@@ -38,11 +43,43 @@ import org.springframework.stereotype.Repository;
     }
 
     /**
-     * @param clusterConfig
+     * @param clusterName
      * @return ClusterConfig
      * @desc get ClusterConfig by cluster name
      */
-    public ClusterConfig getClusterConfig(ClusterConfig clusterConfig) {
-        return null;
+    public ClusterConfig getClusterConfig(String clusterName) {
+        ClusterConfigPO clusterConfigPO = clusterConfigDao.getClusterConfig(clusterName);
+        ClusterConfig clusterConfig = new ClusterConfig();
+        BeanUtils.copyProperties(clusterConfigPO, clusterConfig);
+        return clusterConfig;
+    }
+
+    /**
+     * batch insert
+     *
+     * @param clusterConfigPOList
+     * @return
+     */
+    public boolean batchInsert(List<ClusterConfigPO> clusterConfigPOList) {
+        int affectRows = 0;
+        try {
+            affectRows = clusterConfigDao.batchInsert(clusterConfigPOList);
+        } catch (DuplicateKeyException e) {
+            log.error(
+                "batch insert clusterconfig fail, because there is DuplicateKeyException for clusterConfigPOList:",
+                clusterConfigPOList);
+            throw new SlaveException(SlaveErrorEnum.SLAVE_IDEMPOTENT);
+        }
+        return affectRows == clusterConfigPOList.size();
+    }
+
+    /**
+     * batch update
+     *
+     * @param clusterConfigPOList
+     * @return
+     */
+    public boolean batchUpdate(List<ClusterConfigPO> clusterConfigPOList) {
+        return clusterConfigPOList.size() == clusterConfigDao.batchUpdate(clusterConfigPOList);
     }
 }
