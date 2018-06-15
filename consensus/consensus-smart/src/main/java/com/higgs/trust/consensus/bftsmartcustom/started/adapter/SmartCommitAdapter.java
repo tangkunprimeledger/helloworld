500969f73b47a8b7b11676b352d793cce8381545
zyf <zhouyafeng@primeledger.cn>
2018-06-15 12:03:56 +0800

    移动目录

---
+++ consensus/consensus-smart/src/main/java/com/higgs/trust/consensus/bftsmartcustom/started/adapter/SmartCommitAdapter.java
@@ -0,0 +1,32 @@
+package com.higgs.trust.consensus.bftsmartcustom.started.adapter;
+
+import com.higgs.trust.consensus.core.ConsensusCommit;
+import com.higgs.trust.consensus.core.command.AbstractConsensusCommand;
+
+public class SmartCommitAdapter<T extends AbstractConsensusCommand> implements ConsensusCommit<T> {
+
+    private T command;
+
+    public SmartCommitAdapter(Object object) {
+        if (object instanceof AbstractConsensusCommand) {
+            this.command = (T) object;
+        } else {
+            throw new RuntimeException("the commit is not support!");
+        }
+    }
+
+    @Override
+    public T operation() {
+        return command;
+    }
+
+    @Override
+    public void close() {
+        //do nothing
+    }
+
+    @Override
+    public boolean isClosed() {
+        return false;
+    }
+}
