2c76c40fbcabfee78297b989564b0f2700ef16f2
lingchao <lingchao@primeledger.cn>
2018-06-20 21:45:24 +0800

    fix todo

--- slave/src/test/java/com/higgs/trust/slave/dao/dataIdentity/DataIdentityDaoTest.java
+++ slave/src/test/java/com/higgs/trust/slave/dao/dataIdentity/DataIdentityDaoTest.java
@@ -1,14 +1,18 @@
 package com.higgs.trust.slave.dao.dataIdentity;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.LoadingCache;
 import com.higgs.trust.slave.BaseTest;
 import com.higgs.trust.slave.dao.po.DataIdentityPO;
+import com.higgs.trust.slave.model.bo.snapshot.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * DataIdentityDao test
@@ -42,6 +46,25 @@ public class DataIdentityDaoTest extends BaseTest {
             dataIdentityList.add(dataIdentityPO);
         }
         System.out.println("batchInsertTest：" + dataIdentityDao.batchInsert(dataIdentityList));
+    }
+
+
+    @Test public void loadCache() throws InterruptedException {
+        LoadingCache<Object, Object> bizCache = CacheBuilder.newBuilder().initialCapacity(10).maximumSize(5).refreshAfterWrite(1, TimeUnit.MICROSECONDS).build(new com.google.common.cache.CacheLoader<Object, Object>() {
+            @Override
+            public Object load(Object bo) throws Exception {
+                return "12321"+new Random().nextInt();
+            }
+        });
+
+        for(int i =0; i<20; i++){
+            bizCache.put(i+"",i);
+        }
+        Thread.sleep(10000);
+        System.out.println("================================================");
+        for(Map.Entry<Object, Object> innerEntry : bizCache.asMap().entrySet()){
+            System.out.println("Key="+innerEntry.getKey()+ "     value ="+ innerEntry.getValue());
+        }
     }
 
 }
