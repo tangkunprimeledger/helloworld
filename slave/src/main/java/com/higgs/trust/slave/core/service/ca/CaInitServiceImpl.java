6bf2295b15bbc757cdaec522ad096550f6fec8bd
王全洲 <wangquanzhou@primeledger.cn>
2018-06-21 16:09:50 +0800

    ca init 16点08分

--- slave/src/main/java/com/higgs/trust/slave/core/service/ca/CaInitServiceImpl.java
+++ slave/src/main/java/com/higgs/trust/slave/core/service/ca/CaInitServiceImpl.java
@@ -3,19 +3,22 @@ package com.higgs.trust.slave.core.service.ca;
 import com.higgs.trust.config.node.NodeProperties;
 import com.higgs.trust.config.node.NodeState;
 import com.higgs.trust.config.p2p.ClusterInfo;
+import com.higgs.trust.slave.api.enums.RespCodeEnum;
 import com.higgs.trust.slave.api.vo.RespData;
 import com.higgs.trust.slave.common.enums.SlaveErrorEnum;
 import com.higgs.trust.slave.common.exception.SlaveException;
 import com.higgs.trust.slave.core.repository.config.ConfigRepository;
 import com.higgs.trust.slave.core.service.action.ca.CaInitHandler;
 import com.higgs.trust.slave.integration.ca.CaInitClient;
+import com.higgs.trust.slave.model.bo.action.Action;
+import com.higgs.trust.slave.model.bo.ca.CaAction;
+import com.higgs.trust.slave.model.bo.config.Config;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.mina.util.ConcurrentHashSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -58,16 +61,26 @@ import java.util.concurrent.TimeUnit;
      */
     @Override public void initKeyPair() {
 
-        Map caMap = acquirePubKeys(nodeProperties.getStartupRetryTime());
+        List<Action> caActionList = acquirePubKeys(nodeProperties.getStartupRetryTime());
 
         // construct genius block and insert into db
         try {
             log.info("[CaInitServiceImpl.initKeyPair] start to generate genius block");
-            caInitHandler.process(caMap);
-            log.info("[CaInitServiceImpl.initKeyPair] end generate genius block");
 
-            //            writeKyePairToFile(caMap);
-            log.info("[CaInitServiceImpl.initKeyPair] end write all nodes' pubKey to file");
+            // sort caActionList by user
+            Collections.sort(caActionList, new Comparator<Action>() {
+                @Override public int compare(Action caAction1, Action caAction2) {
+                    return ((CaAction)caAction1).getUser().compareTo(((CaAction)caAction2).getUser());
+                }
+            });
+
+            if(log.isDebugEnabled()){
+                log.debug("[CaInitServiceImpl.initKeyPair] user ={}, caActionList={}", nodeState.getNodeName(),
+                    caActionList.toString());
+            }
+
+            caInitHandler.process(caActionList);
+            log.info("[CaInitServiceImpl.initKeyPair] end generate genius block");
 
         } catch (Throwable e) {
             log.error("[CaInitServiceImpl.initKeyPair] cluster init CA error", e);
@@ -77,28 +90,35 @@ import java.util.concurrent.TimeUnit;
 
     }
 
-    private Map acquirePubKeys(int retryCount) {
+    private List<Action> acquirePubKeys(int retryCount) {
         List<String> nodeList = clusterInfo.clusterNodeNames();
-        Map<String, String> caMap = new HashMap();
+        Set<String> nodeSet = new ConcurrentHashSet<>();
+        List<Action> caActionList = new LinkedList<>();
         log.info(
             "[CaInitServiceImpl.acquirePubKeys] start to acquire all nodes' pubKey, nodeList size = {}, nodeList = {}",
             nodeList.size(), nodeList.toString());
 
         int i = 0;
         do {
-            if (caMap.size() == nodeList.size()) {
-                log.info("[CaInitServiceImpl.acquirePubKeys]  acquired all nodes' pubKey, caMap size = {}",
-                    caMap.size());
-                return caMap;
+            if (nodeSet.size() == nodeList.size()) {
+                log.info("[CaInitServiceImpl.acquirePubKeys]  acquired all nodes' pubKey, nodeSet size = {}",
+                    nodeSet.size());
+                return caActionList;
             }
             CountDownLatch countDownLatch = new CountDownLatch(nodeList.size());
             // acquire all nodes' pubKey
             nodeList.forEach((nodeName) -> {
                 try {
-                    RespData<String> resp = caClient.caInit(nodeName);
-
-                    String pubKey = resp.getData();
-                    caMap.put(nodeName, pubKey);
+                    if (!nodeSet.contains(nodeName)) {
+                        RespData<String> resp = caClient.caInit(nodeName);
+                        if (resp.isSuccess()) {
+                            CaAction caAction = new CaAction();
+                            caAction.setUser(nodeName);
+                            caAction.setPubKey(resp.getData());
+                            caActionList.add(caAction);
+                            nodeSet.add(nodeName);
+                        }
+                    }
                 } catch (Throwable e) {
                     log.error("[CaInitServiceImpl.acquirePubKeys] acquire pubKey error", e);
                 } finally {
@@ -120,23 +140,29 @@ import java.util.concurrent.TimeUnit;
 
         } while (++i < retryCount);
 
-        if (caMap.size() < nodeList.size()) {
+        if (caActionList.size() < nodeList.size()) {
             log.info(
-                "[CaInitServiceImpl.acquirePubKeys]  error acquire all nodes' pubKey, caMap size = {},caMap={}, nodeList.size={},nodeList={}",
-                caMap.size(), caMap.toString(), nodeList.size(), nodeList.toString());
+                "[CaInitServiceImpl.acquirePubKeys]  error acquire all nodes' pubKey, caActionList size = {},caActionList={}, nodeList.size={},nodeList={}",
+                caActionList.size(), caActionList.toString(), nodeList.size(), nodeList.toString());
             throw new SlaveException(SlaveErrorEnum.SLAVE_CA_INIT_ERROR,
                 "[CaInitServiceImpl.acquirePubKeys] cluster init CA error, can not acquire enough pubKeys");
         }
 
-        log.info("[CaInitServiceImpl.acquirePubKeys]  end acquire all nodes' pubKey, caMap size = {}", caMap.size());
-        return caMap;
+        log.info("[CaInitServiceImpl.acquirePubKeys]  end acquire all nodes' pubKey, caActionList size = {}",
+            caActionList.size());
+        return caActionList;
     }
 
     @Override public RespData<String> initCaTx() {
         // TODO 公私钥的生成会在集群自检时，发现没有创世块，那么就应该生成公私钥
 
         // acquire pubKey from DB
-        String pubKey = configRepository.getConfig(nodeState.getNodeName()).getPubKey();
+        Config config = configRepository.getConfig(nodeState.getNodeName());
+        if (null == config) {
+            log.info("[CaInitServiceImpl.initCaTx] nodeName={}, pubKey not exist", nodeState.getNodeName());
+            return new RespData<>(RespCodeEnum.PARAM_NOT_ILLEGAL);
+        }
+        String pubKey = config.getPubKey();
         log.info("[CaInitServiceImpl.initCaTx] nodeName={}, pubKey={}", nodeState.getNodeName(), pubKey);
         RespData resp = new RespData();
         resp.setData(pubKey);
