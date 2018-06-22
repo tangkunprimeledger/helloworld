db04c1abe6658cc03d61b35d146d73ac11057b13
tangfashuang <tangfashuang@primeledger.cn>
2018-06-22 10:49:19 +0800

    move path for request table

---
+++ rs/src/main/java/com/higgs/trust/rs/core/dao/RequestDao.java
@@ -0,0 +1,28 @@
+package com.higgs.trust.rs.core.dao;
+
+import com.higgs.trust.common.mybatis.BaseDao;
+import com.higgs.trust.rs.core.dao.po.RequestPO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface RequestDao extends BaseDao<RequestPO> {
+    /**
+     * 根据requestId 查询请求数据
+     *
+     * @param requestId
+     * @return
+     */
+    RequestPO queryByRequestId(@Param("requestId") String requestId);
+
+
+    /**
+     *
+     * 更改请求状态
+     * @param requestId
+     * @param fromStatus
+     * @param toStatus
+     * @return
+     */
+    int updateStatusByRequestId(@Param("requestId") String requestId, @Param("fromStatus")String fromStatus, @Param("toStatus")String toStatus, @Param("respCode") String respCode, @Param("respMsg") String respMsg);
+}
