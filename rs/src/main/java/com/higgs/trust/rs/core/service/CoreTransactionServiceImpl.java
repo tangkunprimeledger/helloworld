4144d59f9c3309e362f4275798484a870a09727c
liuyu <liuyu@primeledger.cn>
2018-06-25 19:47:19 +0800

    filter self rs for async vote

--- rs/src/main/java/com/higgs/trust/rs/core/service/CoreTransactionServiceImpl.java
+++ rs/src/main/java/com/higgs/trust/rs/core/service/CoreTransactionServiceImpl.java
@@ -310,11 +310,18 @@ import java.util.List;
                 //query receipts by txId
                 List<VoteReceipt> receipts = voteReceiptRepository.queryByTxId(bo.getTxId());
                 if (CollectionUtils.isEmpty(receipts)) {
-                    log.error("[processNeedVoteTx]receipts is empty by txId:{}", bo.getTxId());
+                    log.warn("[processNeedVoteTx]receipts is empty by txId:{}", bo.getTxId());
                     return;
                 }
-                if (receipts.size() < rsIds.size() - 1) {
-                    log.error("[processNeedVoteTx]receipts.size:{} less than rsIds.size:{} by txId:{}", receipts.size(),
+                //filter self
+                List<String> lastRsIds = new ArrayList<>();
+                for(String rsName : rsIds){
+                    if(!StringUtils.equals(rsName,rsConfig.getRsName())){
+                        lastRsIds.add(rsName);
+                    }
+                }
+                if (receipts.size() != lastRsIds.size()) {
+                    log.warn("[processNeedVoteTx]receipts.size:{} less than rsIds.size:{} by txId:{}", receipts.size(),
                         rsIds.size(), bo.getTxId());
                     return;
                 }
