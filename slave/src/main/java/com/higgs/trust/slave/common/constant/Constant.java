e5ac7e56af91047c297fbeecb198d812cf209a73
liuyu <liuyu@primeledger.cn>
2018-07-24 17:08:27 +0800

    modify

--- slave/src/main/java/com/higgs/trust/slave/common/constant/Constant.java
+++ slave/src/main/java/com/higgs/trust/slave/common/constant/Constant.java
@@ -23,12 +23,12 @@ public class Constant {
 
     public static final Long GENESIS_HEIGHT = 1L;
 
-    public static final int MAX_BLOCKING_QUEUE_SIZE = 100000;
+    public static final int MAX_BLOCKING_QUEUE_SIZE = 1000;
 
-    public static final int MAX_PENDING_TX_QUEUE_SIZE = 200000;
+    public static final int MAX_PENDING_TX_QUEUE_SIZE = 20000;
 
-    public static final int MAX_EXIST_MAP_SIZE = 100000;
+    public static final int MAX_EXIST_MAP_SIZE = 50000;
 
-    public static final int PERF_LOG_THRESHOLD = 300000;
+    public static final int PERF_LOG_THRESHOLD = 30000;
 
 }
