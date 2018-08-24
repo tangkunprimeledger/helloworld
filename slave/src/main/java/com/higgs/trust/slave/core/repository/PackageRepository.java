22d89255dee5715b01d8e88c0ba755e76db1560c
liuyu <liuyu@primeledger.cn>
2018-08-24 16:40:16 +0800

    p2p valid handler

--- slave/src/main/java/com/higgs/trust/slave/core/repository/PackageRepository.java
+++ slave/src/main/java/com/higgs/trust/slave/core/repository/PackageRepository.java
@@ -9,7 +9,6 @@ import com.higgs.trust.slave.model.bo.Package;
 import com.higgs.trust.slave.model.enums.biz.PackageStatusEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.TransactionStatus;
@@ -62,12 +61,7 @@ import java.util.Set;
             @Override protected void doInTransactionWithoutResult(TransactionStatus status) {
                 int r = packageDao.updateStatus(height, from.getCode(), to.getCode());
                 if (r != 1) {
-                    //check package status
-                    PackagePO po = packageDao.queryByHeight(height);
-                    if (null != po && StringUtils.equals(to.getCode(), po.getStatus())) {
-                        return;
-                    }
-                    log.error("[package.updateStatus] has error");
+                    log.error("package.updateStatus is fail from:{} to:{}",from,to);
                     throw new SlaveException(SlaveErrorEnum.SLAVE_PACKAGE_UPDATE_STATUS_ERROR);
                 }
             }
