f750221a23f2c948faf83e27dbe90af0a9ec5133
68d_100008064392_liuyu <68d_100008064392_liuyu@git.cloud.tencent.com>
2018-11-09 11:21:39 +0800

    lottery

--- contract/src/main/java/com/higgs/trust/contract/rhino/function/PrintNativeFunction.java
+++ contract/src/main/java/com/higgs/trust/contract/rhino/function/PrintNativeFunction.java
@@ -1,6 +1,7 @@
 package com.higgs.trust.contract.rhino.function;
 
 import com.higgs.trust.contract.rhino.types.BigDecimalWrap;
+import lombok.extern.slf4j.Slf4j;
 import org.mozilla.javascript.BaseFunction;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
@@ -10,6 +11,7 @@ import org.mozilla.javascript.Undefined;
  * @author duhongming
  * @date 2018/6/6
  */
+@Slf4j
 public class PrintNativeFunction extends BaseFunction {
 
     @Override
@@ -17,6 +19,7 @@ public class PrintNativeFunction extends BaseFunction {
         Object o = args[0];
         String s = o instanceof Number || o instanceof BigDecimalWrap ? o.toString() : Context.toString(o);
         System.out.println(s);
+        log.info(s);
         return Undefined.instance;
     }
 }
