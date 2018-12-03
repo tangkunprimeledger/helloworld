6418d0f64bcaf089bcd69157f16f724ab4d0fee3
lingchao <lingchao@primeledger.cn>
2018-12-03 16:35:59 +0800

    add logic for request table

--- rs/src/main/java/com/higgs/trust/rs/core/repository/RequestRepository.java
+++ rs/src/main/java/com/higgs/trust/rs/core/repository/RequestRepository.java
@@ -1,17 +1,18 @@
 package com.higgs.trust.rs.core.repository;
 
-import com.alibaba.fastjson.JSON;
 import com.higgs.trust.rs.common.config.RsConfig;
 import com.higgs.trust.rs.common.enums.RequestEnum;
-import com.higgs.trust.rs.common.enums.RespCodeEnum;
+import com.higgs.trust.rs.common.enums.RsCoreErrorEnum;
+import com.higgs.trust.rs.common.exception.RsCoreException;
 import com.higgs.trust.rs.core.dao.RequestDao;
 import com.higgs.trust.rs.core.dao.RequestJDBCDao;
 import com.higgs.trust.rs.core.dao.po.RequestPO;
 import com.higgs.trust.rs.core.dao.rocks.RequestRocksDao;
+import com.higgs.trust.rs.core.vo.RequestVO;
 import com.higgs.trust.rs.core.vo.RsCoreTxVO;
 import com.higgs.trust.slave.api.vo.RespData;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Repository;
@@ -38,25 +39,38 @@ public class RequestRepository {
 
     /**
      * check request idempotent
+     *
      * @param requestId
      * @return
      */
-    public RespData requestIdempotent(String requestId) {
-
+    public RespData<?> requestIdempotent(String requestId) {
+        RespData<?> respData = null;
         RequestPO requestPO;
         if (rsConfig.isUseMySQL()) {
             requestPO = requestDao.queryByRequestId(requestId);
         } else {
             requestPO = requestRocksDao.get(requestId);
         }
-
         if (null != requestPO) {
-            if (StringUtils.equals(RequestEnum.PROCESS.getCode(), requestPO.getStatus())) {
-                return new RespData(RespCodeEnum.REQUEST_DUPLICATE.getRespCode(), RespCodeEnum.REQUEST_DUPLICATE.getMsg());
-            }
-            return new RespData<>(requestPO.getRespCode(), requestPO.getRespMsg());
+            respData = new RespData<>(requestPO.getRespCode(), requestPO.getRespMsg());
         }
-        return null;
+        return respData;
+    }
+
+    /**
+     * check request idempotent
+     *
+     * @param requestId
+     * @return
+     */
+    public RequestVO queryByRequestId(String requestId) {
+        RequestPO requestPO = requestDao.queryByRequestId(requestId);
+        if (null == requestPO) {
+            return null;
+        }
+        RequestVO requestVO = new RequestVO();
+        BeanUtils.copyProperties(requestPO, requestVO);
+        return requestVO;
     }
 
     /**
@@ -64,34 +78,82 @@ public class RequestRepository {
      *
      * @param
      */
-    public RespData insertRequest(String requestId, Object obj) {
-        RespData respData = null;
-        if (rsConfig.isUseMySQL()) {
-            try {
-                requestDao.add(buildRequestPO(requestId, obj));
-            } catch (DuplicateKeyException e) {
-                log.error("requestId : {} for : obj {} is idempotent", requestId, obj);
-                respData = requestIdempotent(requestId);
+    public RespData<?> insertRequest(String requestId, RequestEnum requestEnum, String respCode, String respMsg) {
+        RespData<?> respData = null;
+        RequestPO requestPO = buildRequestPO(requestId, requestEnum, respCode, respMsg);
+        try {
+            //for mysql
+            if (rsConfig.isUseMySQL()) {
+                requestDao.add(requestPO);
+                return null;
             }
-        } else {
-            requestRocksDao.save(buildRequestPO(requestId, obj));
+            //for rocks DB
+            requestRocksDao.save(requestPO);
+        } catch (DuplicateKeyException e) {
+            log.error("Request for requestId : {} is idempotent", requestId);
+            respData = requestIdempotent(requestId);
         }
         return respData;
     }
 
-    public void updateRequest(String requestId, RequestEnum fromStatus, RequestEnum toStatus, String respCode, String respMsg) {
-        if (rsConfig.isUseMySQL()) {
-            requestDao.updateStatusByRequestId(requestId, fromStatus.getCode(), toStatus.getCode(), respCode, respMsg);
-        } else {
-            requestRocksDao.updateStatus(requestId, fromStatus.getCode(), toStatus.getCode(), respCode, respMsg);
-        }
+
+    /**
+     * just update code
+     *
+     * @param requestId
+     * @param respCode
+     * @param respMsg
+     */
+    public void updateCode(String requestId, String respCode, String respMsg) {
+        update(requestId, null, null, respCode, respMsg);
     }
 
-    private RequestPO buildRequestPO(String requestId, Object obj) {
+
+    /**
+     * update status and code
+     *
+     * @param requestId
+     * @param fromStatus
+     * @param toStatus
+     * @param respCode
+     * @param respMsg
+     */
+    public void updateStatusAndCode(String requestId, RequestEnum fromStatus, RequestEnum toStatus, String respCode, String respMsg) {
+        update(requestId, fromStatus, toStatus, respCode, respMsg);
+    }
+
+    /**
+     * update
+     *
+     * @param requestId
+     * @param fromStatus
+     * @param toStatus
+     * @param respCode
+     * @param respMsg
+     */
+    private void update(String requestId, RequestEnum fromStatus, RequestEnum toStatus, String respCode, String respMsg) {
+        String fromStatusCode = null != fromStatus ? fromStatus.getCode() : null;
+        String toStatusCode = null != toStatus ? toStatus.getCode() : null;
+        //for mysql
+        if (rsConfig.isUseMySQL()) {
+            int isUpdated = requestDao.updateStatusByRequestId(requestId, fromStatusCode, toStatusCode, respCode, respMsg);
+            if (1 != isUpdated) {
+                log.error("Update request failed for requestId :{}, fromStatus :{}, toStatus :{}, respCode :{}, respMsg :{}", requestId, fromStatus.getCode(), toStatus.getCode(), respCode, respMsg);
+                throw new RsCoreException(RsCoreErrorEnum.RS_CORE_REQUEST_UPDATE_STATUS_FAILED);
+            }
+            return;
+        }
+
+        //for rockDB
+        requestRocksDao.updateStatus(requestId, fromStatusCode, toStatusCode, respCode, respMsg);
+    }
+
+    private RequestPO buildRequestPO(String requestId, RequestEnum requestEnum, String respCode, String respMsg) {
         RequestPO requestPO = new RequestPO();
         requestPO.setRequestId(requestId);
-        requestPO.setData(JSON.toJSONString(obj));
-        requestPO.setStatus(RequestEnum.PROCESS.getCode());
+        requestPO.setRespCode(respCode);
+        requestPO.setRespMsg(respMsg);
+        requestPO.setStatus(requestEnum.getCode());
         return requestPO;
     }
 
---
+++ rs/src/test/java/com/higgs/trust/rs/core/dao/RequestDaoTest.java
@@ -0,0 +1,45 @@
+package com.higgs.trust.rs.core.dao;
+
+import com.higgs.trust.IntegrateBaseTest;
+import com.higgs.trust.rs.common.enums.RequestEnum;
+import com.higgs.trust.rs.core.dao.po.RequestPO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * @description: com.higgs.trust.rs.core.dao
+ * @author: lingchao
+ * @datetime:2018年12月03日14:32
+ **/
+public class RequestDaoTest extends IntegrateBaseTest {
+    private String requestId = System.currentTimeMillis() + "";
+    @Autowired
+    private RequestDao requestDao;
+
+    @Test
+    public void add() {
+        RequestPO requestPO = new RequestPO();
+        requestPO.setRequestId(requestId);
+        requestPO.setStatus(RequestEnum.PROCESS.getCode());
+        requestPO.setRespCode("000009");
+        requestPO.setRespMsg("INIT");
+        System.out.println(requestDao.add(requestPO));
+        System.out.println(requestDao.queryByRequestId(requestId));
+        System.out.println(requestDao.updateStatusByRequestId(requestId,null, null, "000001", "PROCESS"));
+        System.out.println(requestDao.queryByRequestId(requestId));
+
+    }
+
+
+    @Test
+    public void testQueryByRequestId() {
+        System.out.println(requestDao.queryByRequestId(requestId));
+    }
+
+    @Test
+    public void testUpdateStatusByRequestId() {
+        System.out.println(requestDao.updateStatusByRequestId(requestId, RequestEnum.PROCESS.getCode(), RequestEnum.DONE.getCode(), "000000", "OK"));
+    }
+}
