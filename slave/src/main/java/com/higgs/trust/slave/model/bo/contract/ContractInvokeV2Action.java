069e76481486a9128b3793f575abce727d2a6bd9
kongyu <kongyu@primeledger>
2018-12-10 10:33:26 +0800

    新增action字段校验

--- slave/src/main/java/com/higgs/trust/slave/model/bo/contract/ContractInvokeV2Action.java
+++ slave/src/main/java/com/higgs/trust/slave/model/bo/contract/ContractInvokeV2Action.java
@@ -3,7 +3,9 @@ package com.higgs.trust.slave.model.bo.contract;
 import com.higgs.trust.slave.model.bo.action.Action;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
@@ -17,6 +19,8 @@ public class ContractInvokeV2Action extends Action {
     /**
      * contract address
      */
+    @NotNull
+    @NotBlank
     private String address;
     /**
      * if transfer，which is tx's nonce
@@ -29,8 +33,12 @@ public class ContractInvokeV2Action extends Action {
     /**
      * 调用方法签名(方法名+参数列表+返回值，例如：(uint) balanceOf(address))
      */
+    @NotNull
+    @NotBlank
     private String methodSignature;
 
-    /**智能合约调用传入参数列表*/
+    /**
+     * 智能合约调用传入参数列表
+     */
     private Object[] args;
 }
