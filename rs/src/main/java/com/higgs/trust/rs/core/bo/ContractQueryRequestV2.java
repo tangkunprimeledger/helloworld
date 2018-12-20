103ae55c95419247785c190a3dd557d57ead8ece
chenjiaweiprimeledger <chenjiawei@primeledger.cn>
2018-12-20 10:47:32 +0800

    modify code.

--- rs/src/main/java/com/higgs/trust/rs/core/bo/ContractQueryRequestV2.java
+++ rs/src/main/java/com/higgs/trust/rs/core/bo/ContractQueryRequestV2.java
@@ -13,7 +13,7 @@ public class ContractQueryRequestV2 {
     /**
      * Height of block.
      */
-    private int blockHeight;
+    private long blockHeight;
     /**
      * Contract address, hex string of 40 characters, e.g. 00a615668486da40f31fd050854fb137b317e056.
      */
