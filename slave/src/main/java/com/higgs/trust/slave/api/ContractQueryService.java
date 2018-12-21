bd8f9817af67966f2e8561d19b2813571db53218
chenjiaweiprimeledger <chenjiawei@primeledger.cn>
2018-12-21 11:31:22 +0800

    add contract querying override method.

--- slave/src/main/java/com/higgs/trust/slave/api/ContractQueryService.java
+++ slave/src/main/java/com/higgs/trust/slave/api/ContractQueryService.java
@@ -20,11 +20,40 @@ public interface ContractQueryService {
     /**
      * Queries contract state.
      *
-     * @param blockHeight block height
+     * @param blockHeight     block height
      * @param contractAddress contract address
      * @param methodSignature method signature written with target language
      * @param methodInputArgs actual parameters
      * @return result returned by contract invocation
      */
     List<?> query2(long blockHeight, String contractAddress, String methodSignature, Object... methodInputArgs);
+
+    /**
+     * Queries contract state.
+     *
+     * @param blockHeight     block height
+     * @param contractAddress contract address
+     * @param methodSignature method signature written with target language
+     * @param methodInputArgs actual parameters
+     * @return result returned by contract invocation
+     */
+    default List<?> query2(Long blockHeight, String contractAddress, String methodSignature, Object... methodInputArgs) {
+        long height = -1;
+        if (blockHeight > 0) {
+            height = blockHeight;
+        }
+        return query2(height, contractAddress, methodSignature, methodInputArgs);
+    }
+
+    /**
+     * Queries contract state.
+     *
+     * @param contractAddress contract address
+     * @param methodSignature method signature written with target language
+     * @param methodInputArgs actual parameters
+     * @return result returned by contract invocation
+     */
+    default List<?> query2(String contractAddress, String methodSignature, Object... methodInputArgs) {
+        return query2(-1, contractAddress, methodSignature, methodInputArgs);
+    }
 }
