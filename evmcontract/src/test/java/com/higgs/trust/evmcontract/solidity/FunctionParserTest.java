3d4d8bb7300f994edf1adc2b81f64c241dab22b0
chenjiaweiprimeledger <chenjiawei@primeledger.cn>
2019-01-02 15:13:52 +0800

    modify test code.

--- evmcontract/src/test/java/com/higgs/trust/evmcontract/solidity/FunctionParserTest.java
+++ evmcontract/src/test/java/com/higgs/trust/evmcontract/solidity/FunctionParserTest.java
@@ -111,160 +111,60 @@ public class FunctionParserTest {
     @Test(expected = IllegalArgumentException.class)
     public void testParse_08() {
         String functionSignature = "(uint, address, uint) run pay(address, bytes32)";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testParse_09() {
         String functionSignature = "(ui nt, address, uint) run(address, bytes32)";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testParse_10() {
         String functionSignature = "(uint, address, uint) run(addr ess, bytes32)";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testParse_11() {
         String functionSignature = "(uint, address, uint)() run(address, bytes32)";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testParse_12() {
         String functionSignature = "(uint, address, uint) run()(address, bytes32)";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testParse_13() {
         String functionSignature = "(uint, address, uint) run(address, -bytes32)";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testParse_14() {
         String functionSignature = "(uint, ) run(address, bytes32)";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testParse_15() {
         String functionSignature = "(uint, address, uint) run(address, bytes32) pay()";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testParse_16() {
         String functionSignature = "run(uint, address, uint) (address, bytes32)";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testParse_17() {
         String functionSignature = "(uint, address,, uint) run(address, bytes32)";
-        Abi.Function function = functionParser.parse(functionSignature);
-
-        assertEquals(Abi.Entry.Type.function, function.type);
-        assertEquals("run", function.name);
-        assertEquals(2, function.inputs.size());
-        assertEquals("address", function.inputs.get(0).type.getName());
-        assertEquals("bytes32", function.inputs.get(1).type.getName());
-        assertEquals(3, function.outputs.size());
-        assertEquals("uint", function.outputs.get(0).type.getName());
-        assertEquals("address", function.outputs.get(1).type.getName());
-        assertEquals("uint", function.outputs.get(2).type.getName());
+        functionParser.parse(functionSignature);
     }
 }
